43f48f807ce45da5ee2d9f5f3b8abc725c96efdc
CHANHONGYU <1948988304@qq.com>
2020-07-16 21:16:51 +0800

    7-14 19:40 7-15 17:001 7-16

--- src/main/java/com/smart/cmsystem/controller/ComplaintsController.java
+++ src/main/java/com/smart/cmsystem/controller/ComplaintsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.List;
 
+
 @RestController
 @CrossOrigin(origins = "*" ,maxAge = 3600)
 @RequestMapping("/complaints")
---
+++ src/test/java/com/smart/cmsystem/service/impl/ComplaintsServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.smart.cmsystem.service.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ComplaintsServiceImplTest {
+
+    @Test
+    public void deleteComplaintsById() {
+
+    }
+}
