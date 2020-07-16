2310e3222205006a61f1f50d82d4a06203f22433
CHANHONGYU <1948988304@qq.com>
2020-07-16 23:18:09 +0800

    7-14 19:40 7-15 17:001 7-16

--- src/main/java/com/smart/cmsystem/utils/ResponseEntity.java
+++ src/main/java/com/smart/cmsystem/utils/ResponseEntity.java
@@ -2,42 +2,69 @@ package com.smart.cmsystem.utils;
 
 import lombok.Data;
 
+import java.io.Serializable;
 
 @Data
-public class ResponseEntity<T> {
+public class ResponseEntity<T> implements Serializable {
     private int status;
     private String msg;
     private T data;
 
     /**
-     * 在这里定义静态方法,就不需要自己在其它层用set方法一步一步设置值,就是封装
+     * 成功默认的执行方法
+     *
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static <T> ResponseEntity<T> success(T data) {
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setData(data);
+        responseEntity.setStatus(ErrorStatus.SUCCESS.getStatus());
+        responseEntity.setMsg(ErrorStatus.SUCCESS.getMsg());
+        return responseEntity;
+    }
+
+    /**
+     * 指定状态的成功方法
      * @param status
      * @param data
      * @param <T>
      * @return
      */
-    public static <T> ResponseEntity<T> success(ErrorStatus status, T data) {
-        ResponseEntity<T> entity = new ResponseEntity<>();
-        entity.setStatus(status.getStatus());
-        entity.setMsg(status.getMsg());
-        entity.setData(data);
-        return entity;
-
-
+    public static <T> ResponseEntity<T> success(ErrorStatus status,T data) {
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setData(data);
+        responseEntity.setStatus(status.getStatus());
+        responseEntity.setMsg(status.getMsg());
+        return responseEntity;
     }
-    public static <T> ResponseEntity<T> success(ErrorStatus status) {
-        ResponseEntity<T> entity = new ResponseEntity<>();
-        entity.setStatus(status.getStatus());
-        entity.setMsg(status.getMsg());
-        return entity;
 
-
+    /**
+     * 默认失败方法
+     * @param <T>
+     * @return
+     */
+    public static <T> ResponseEntity<T> error() {
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setMsg(ErrorStatus.SYS_ERROR.getMsg());
+        responseEntity.setStatus(ErrorStatus.SYS_ERROR.getStatus());
+        responseEntity.setData(new Object());
+        return responseEntity;
     }
+
+    /**
+     * 指定状态的失败方法
+     * @param <T>
+     * @return
+     */
 
     public static <T> ResponseEntity<T> error(ErrorStatus status) {
-        ResponseEntity<T> entity = new ResponseEntity<>();
-        entity.setStatus(status.getStatus());
-        entity.setMsg(status.getMsg());
-        return entity;
+        ResponseEntity responseEntity = new ResponseEntity();
+        responseEntity.setMsg(status.getMsg());
+        responseEntity.setStatus(status.getStatus());
+        responseEntity.setData(new Object());
+        return responseEntity;
     }
+
 }
